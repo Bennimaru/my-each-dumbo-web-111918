b9cf174741642f5a0549dd4100d22d0aac80055c
Benson Yu <bennywhyu@gmail.com>
2018-11-14 01:05:34 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,13 @@ def my_each (array)# put argument(s) here
   # code here
   while i=0 
   
-    if i<array.length 
+  if i<array.length 
     yield array[i]
     i=i+1
-    end 
-  return array
   end 
+  
+  return array
+  
+  end 
+
 end
